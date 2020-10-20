b987ba3a9064a3847bc8e667af1dfed4532a4aa6
Sui Chen <suichen@google.com>
2020-10-20 13:51:03 -0700

    become monitor

--- Makefile
+++ Makefile
@@ -1,25 +1,23 @@
 all: dbus-top
 
 # Edit the following two lines for use with BMC
-# Note: because hwmon_bmk_bmc depends on libboost_system, you might need to copy
-# its runtime library to the BMC as well.
 S=$(HOME)/oecore-x86_64/environment-setup-armv7a-openbmc-linux-gnueabi
 BMC_SYS_ROOT="$(HOME)/oecore-x86_64/sysroots/armv7a-openbmc-linux-gnueabi"
 
 BMC_INCS=-I$(BMC_SYS_ROOT)/usr/include/
-BMC_LIBS=$(HOME)/Downloads/AsioUring/build_bmc/libasio_uring_asio.a $(HOME)/Downloads/AsioUring/build_bmc/libasio_uring_core.a $(HOME)/Downloads/liburing-0.1/build_bmc/liburing.a
+BMC_LIBS=-lsystemd
 
-# ASIO_URing
-ASIO_URING_CFLAGS=-DBOOST_ALL_NO_LIB -DBOOST_SYSTEM_DYN_LINK -I$(HOME)/Downloads/AsioUring/src/asio/include -I$(HOME)/Downloads/AsioUring/src/core/include -isystem $(HOME)/Downloads/liburing-0.1/src/include  -std=gnu++17 $(HOME)/Downloads/AsioUring/build/bin/libasio_uring_asio.a $(HOME)/Downloads/AsioUring/build/bin/libasio_uring_core.a -lpthread /usr/lib/x86_64-linux-gnu/libboost_system.so.1.71.0 $(HOME)/Downloads/liburing-0.1/build/liburing.a
-ASIO_URING_CFLAGS_BMC=--sysroot=$(BMC_SYS_ROOT) -march=armv7-a -DBOOST_ALL_NO_LIB -DBOOST_SYSTEM_DYN_LINK -I$(HOME)/Downloads/AsioUring/src/asio/include -I$(HOME)/Downloads/AsioUring/src/core/include -isystem $(HOME)/Downloads/liburing-0.1/src/include -std=gnu++17 -lpthread -lboost_system
+CFLAGS=-std=c++17 -lsystemd -lpthread
+CFLAGS_BMC=--sysroot=$(BMC_SYS_ROOT) -march=armv7-a -std=gnu++17  -lsystemd -lpthread
 
 dbus-top: main.cpp
-	g++ main.cpp -std=c++17 -O2 -o $@
+	g++ main.cpp -std=c++17 -lsystemd -O2 -o $@
 
 dbus-top_bmc: main.cpp
 	@source $(S) && \
-	 arm-openbmc-linux-gnueabi-g++ $(BMC_INCS) $(ASIO_URING_CFLAGS_BMC) $^ $(BMC_LIBS) -o $@ -O2
+	 arm-openbmc-linux-gnueabi-g++ $(CFLAGS_BMC) $(BMC_INCS) $^ $(BMC_LIBS) -o $@ -O2
 
 
 clean:
-	@if [ -f dbus-top ]; then rm -v dbus-top; fi
+	@if [ -f dbus-top     ]; then rm -v dbus-top    ; fi
+	@if [ -f dbus-top_bmc ]; then rm -v dbus-top_bmc; fi
--- main.cpp
+++ main.cpp
@@ -1,5 +1,109 @@
+// 2020-10-18
+
 #include <stdio.h>
+#include <systemd/sd-bus.h>
+#include <chrono>
+
+std::chrono::time_point<std::chrono::steady_clock> g_t0;
+long ElapsedMillis() {
+	std::chrono::time_point<std::chrono::steady_clock> t = std::chrono::steady_clock::now();
+	return std::chrono::duration_cast<std::chrono::milliseconds>(t - g_t0).count();
+}
+
+// Run this tool with SUDO
+
+bool IS_USER_BUS = false;
+sd_bus* g_bus = nullptr;
+
+int AcquireBus(sd_bus** ret) {
+
+	int r;
+	r = sd_bus_new(&g_bus);
+	if (r < 0) { printf("Could not allocate bus: %s\n", strerror(-r)); return 0; }
+
+	r = sd_bus_set_monitor(g_bus, true);
+	if (r < 0) { printf("Could not set monitor mode: %s\n", strerror(-r)); return 0; }
+
+	r = sd_bus_negotiate_creds(g_bus, true, _SD_BUS_CREDS_ALL);
+	if (r < 0) { printf("Could not enable credentials: %s\n", strerror(-r)); return 0; }
+
+	r = sd_bus_negotiate_timestamp(g_bus, true);
+	if (r < 0) { printf("Could not enable timestamps: %s\n", strerror(-r)); return 0; }
+
+	r = sd_bus_negotiate_fds(g_bus, true);
+	if (r < 0) { printf("Could not enable fds: %s\n", strerror(-r)); return 0; }
+
+	r = sd_bus_set_bus_client(g_bus, true);
+	if (r < 0) { printf("Could not enable bus client: %s\n", strerror(-r)); return 0; }
+	
+	if (IS_USER_BUS) {
+		r = sd_bus_set_address(g_bus, "haha");
+		if (r < 0) { printf("Could not set user bus: %s\n", strerror(-r)); return 0; }
+	} else {
+		r = sd_bus_set_address(g_bus, "unix:path=/run/dbus/system_bus_socket");
+		if (r < 0) { printf("Could not set system bus: %s\n", strerror(-r)); return 0; }
+	}
+
+	r = sd_bus_start(g_bus);
+	if (r < 0) { printf("Could not connect to bus: %s\n", strerror(-r)); return 0; }
+
+	return r;
+}
 
 int main() {
-	printf("dbus-top\n");
+	g_t0 = std::chrono::steady_clock::now();
+
+	uint32_t flags = 0;
+	int r;
+	AcquireBus(&g_bus);
+	sd_bus_message* message;
+	sd_bus_error error = SD_BUS_ERROR_NULL;
+	r = sd_bus_message_new_method_call(g_bus, &message,
+			"org.freedesktop.DBus",
+			"/org/freedesktop/DBus",
+			"org.freedesktop.DBus.Monitoring",
+			"BecomeMonitor");
+	if (r < 0) { printf("Could not create the BecomeMonitor function call\n"); return 0; }
+
+	// Match conditions
+	{
+		r = sd_bus_message_open_container(message, 'a', "s");
+		if (r < 0) { printf("Could not open container\n"); return 0; }
+
+		r = sd_bus_message_close_container(message);
+		if (r < 0) { printf("Could not close container\n"); return 0; }
+	}
+
+	r = sd_bus_message_append_basic(message, 'u', &flags);
+	if (r < 0) { printf("Could not append flags\n"); return 0; }
+
+	r = sd_bus_call(g_bus, message, 0, &error, nullptr);
+	if (r < 0) { printf("Could not call org.freedesktop.DBus.Monitoring.BecomeMonitor %d, %s\n", r, strerror(-r)); return 0; }
+
+	const char* unique_name;
+	r = sd_bus_get_unique_name(g_bus, &unique_name);
+	if (r < 0) { printf("Could not get unique name: %s\n", strerror(-r)); return 0; }
+	printf("Unique name: %s\n", unique_name);
+
+	printf("Monitoring bus stream\n");
+	while (true) {
+		struct sd_bus_message* m = nullptr;
+		r = sd_bus_process(g_bus, &m);
+		printf("%ld ms: ", ElapsedMillis());
+
+		const char* path = sd_bus_message_get_path(m);
+		if (path) printf("Path: %s ", path);
+
+		const char* iface = sd_bus_message_get_interface(m);
+		if (iface) printf("Interface: %s ", iface);
+
+		const char* destination = sd_bus_message_get_destination(m);
+		if (destination) printf("Dest: %s ", destination);
+
+		printf("\n");
+		r = sd_bus_wait(g_bus, (uint64_t)(-1));
+		if (r < 0) { printf("Failed to wait on bus: %s\n", strerror(-r)); }
+	}
+
+	return 0;
 }
