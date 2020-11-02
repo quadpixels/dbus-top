9778a134b55aa98a3b10432b663b199f9cb7f172
Sui Chen <suichen@google.com>
2020-11-02 14:41:05 -0800

    Count number of messages per second

--- Makefile
+++ Makefile
@@ -5,13 +5,13 @@ S=$(HOME)/oecore-x86_64/environment-setup-armv7a-openbmc-linux-gnueabi
 BMC_SYS_ROOT="$(HOME)/oecore-x86_64/sysroots/armv7a-openbmc-linux-gnueabi"
 
 BMC_INCS=-I$(BMC_SYS_ROOT)/usr/include/
-BMC_LIBS=-lsystemd
+BMC_LIBS=-lsystemd -lncurses
 
-CFLAGS=-std=c++17 -lsystemd -lpthread
-CFLAGS_BMC=--sysroot=$(BMC_SYS_ROOT) -march=armv7-a -std=gnu++17  -lsystemd -lpthread
+CFLAGS=-std=c++17 -lsystemd -lpthread -lncurses
+CFLAGS_BMC=--sysroot=$(BMC_SYS_ROOT) -march=armv7-a -std=gnu++17  -lsystemd -lpthread -lncurses
 
 dbus-top: main.cpp
-	g++ main.cpp -std=c++17 -lsystemd -O2 -o $@
+	g++ main.cpp -std=c++17 $(CFLAGS) -O2 -o $@
 
 dbus-top_bmc: main.cpp
 	@source $(S) && \
--- main.cpp
+++ main.cpp
@@ -3,11 +3,57 @@
 #include <stdio.h>
 #include <systemd/sd-bus.h>
 #include <chrono>
+#include <thread>
+#include <unistd.h>
+#include <ncurses.h>
+
+int WIN_W, WIN_H;
 
 std::chrono::time_point<std::chrono::steady_clock> g_t0;
 long ElapsedMillis() {
 	std::chrono::time_point<std::chrono::steady_clock> t = std::chrono::steady_clock::now();
 	return std::chrono::duration_cast<std::chrono::milliseconds>(t - g_t0).count();
+}
+
+// Statistics
+class Stat {
+public:
+	Stat() {
+		NextInterval();
+	}
+	void OnNewMessage(sd_bus_message* m) {
+		num_messages ++;
+	}
+	void NextInterval() {
+		num_messages = 0;
+	}
+	void Print() {
+		for (int i=0; i<WIN_H; i++) {
+			mvprintw(i, 0, "%d", i);
+		}
+		mvprintw(WIN_H-2, 4, "%d messages/s\n", num_messages);
+		refresh();
+	}
+private:
+	int num_messages;
+};
+
+Stat g_stat;
+
+void PollThread() {
+	std::chrono::time_point<std::chrono::steady_clock> t0;
+	std::chrono::seconds one_sec(1);
+	while (true) {
+		std::chrono::time_point<std::chrono::steady_clock> next_expiry = t0, now = std::chrono::steady_clock::now();
+		while (next_expiry < now) {
+			next_expiry = next_expiry + one_sec;
+		}
+
+		long usec = std::chrono::duration_cast<std::chrono::microseconds>(next_expiry - now).count();
+		usleep(usec);
+		g_stat.Print();
+		g_stat.NextInterval();
+	}
 }
 
 // Run this tool with SUDO
@@ -49,8 +95,15 @@ int AcquireBus(sd_bus** ret) {
 
 	return r;
 }
-
+ 
 int main() {
+
+	initscr();
+	getmaxyx(stdscr, WIN_H, WIN_W);
+	mvprintw(WIN_H-2, 2, "Window size: %d x %d\n", WIN_W, WIN_H);
+
+	getch();
+
 	g_t0 = std::chrono::steady_clock::now();
 
 	uint32_t flags = 0;
@@ -83,27 +136,47 @@ int main() {
 	const char* unique_name;
 	r = sd_bus_get_unique_name(g_bus, &unique_name);
 	if (r < 0) { printf("Could not get unique name: %s\n", strerror(-r)); return 0; }
-	printf("Unique name: %s\n", unique_name);
+//	printf("Unique name: %s\n", unique_name);
 
-	printf("Monitoring bus stream\n");
+//	printf("Monitoring bus stream\n");
+
+	std::thread poll_thread(PollThread);
+
 	while (true) {
 		struct sd_bus_message* m = nullptr;
 		r = sd_bus_process(g_bus, &m);
-		printf("%ld ms: ", ElapsedMillis());
+//		printf("%ld ms: ", ElapsedMillis());
 
-		const char* path = sd_bus_message_get_path(m);
-		if (path) printf("Path: %s ", path);
+		if (false) {
+			uint8_t type;
+			r = sd_bus_message_get_type(m, &type);
+			if (r < 0) { printf("Could not get message type: %s\n", strerror(-r)); }
+			else printf("Type=%d ", int(type));
 
-		const char* iface = sd_bus_message_get_interface(m);
-		if (iface) printf("Interface: %s ", iface);
+			const char* path = sd_bus_message_get_path(m);
+			if (path) printf("Path=%s ", path);
 
-		const char* destination = sd_bus_message_get_destination(m);
-		if (destination) printf("Dest: %s ", destination);
+			const char* iface = sd_bus_message_get_interface(m);
+			if (iface) printf("Interface=%s ", iface);
 
-		printf("\n");
+			const char* sender = sd_bus_message_get_sender(m);
+			if (sender) printf("Sender=%s ", sender);
+
+			const char* destination = sd_bus_message_get_destination(m);
+			if (destination) printf("Dest=%s ", destination);
+
+			printf("\n");
+		}
+
+		g_stat.OnNewMessage(m);
+
 		r = sd_bus_wait(g_bus, (uint64_t)(-1));
 		if (r < 0) { printf("Failed to wait on bus: %s\n", strerror(-r)); }
 	}
 
 	return 0;
+}
+
+__attribute__((destructor)) void ResetWindowMode() {
+	endwin();
 }
